d1e1600010b59aed10895991af3bd34527848cbf
josepy333 <josepy333@gmail.com>
2017-09-11 01:13:40 -0500

    Laid out the shell of my methods and comments above each one

--- Assig2.java
+++ Assig2.java
@@ -5,15 +5,62 @@
  *
  */
 
-import java.util.Scanner;
+import java.util.*;
+import java.lang.Math;
 
 public class Assig2
 {
-
-   public static void main(String[] args)
+   /* This prompts the user for input and returns the bet amount as a functional
+    * return.  It should validate the amount before it returns and insist on a 
+    * legal bet (0 < bet < 100) until it gets one from the user.  It must return
+    * the legal value to the client and not take any other action besides 
+    * getting the legal amount.
+    */
+   public static int getBet()
    {
-      // TODO Auto-generated method stub
-
+      
+   }
+   
+   // This method instantiates and returns a TripleString object to the client.
+   public static TripleString pull()
+   {
+      
+   }
+   
+   /* This method produces and returns a single random string based on the
+    * required probabilities.
+    */
+   private String randString()
+   {
+      
+   }
+   
+   /*  Takes the pullString as a parameter, and inspects it to determine what 
+    *  its pay multiplier should be
+    */
+   public int getPayMultiplier(TripleString thePull)
+   {
+      
+   }
+   
+   /* This method takes the winnings (a dollar amount) and thePull as parameters
+    * and displays the three strings inside thePull along with "  sorry - you 
+    * lost " or "congrats, you won $X".
+    */
+   public static void display(TripleString thePull, int winnings)
+   {
+      
    }
 
+   // Main method
+   public static void main(String[] args)
+   {
+      
+   }
+
+}
+
+class TripleString
+{
+   
 }

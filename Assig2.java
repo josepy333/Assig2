088e3d75e052c816fd104a68922e814a5bbb06f6
josepy333 <josepy333@gmail.com>
2017-09-11 00:38:46 -0500

    added Assig2.java

---
+++ Assig2.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ * Joseph Cortez
+ * Assig2 Class: Simulates a slot machine
+ *
+ */
+
+import java.util.Scanner;
+
+public class Assig2
+{
+
+   public static void main(String[] args)
+   {
+      // TODO Auto-generated method stub
+
+   }
+
+}
